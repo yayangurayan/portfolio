8a7e40f0eaca1c914a2e613972504a0536a30a0d
yayangurayan <andriandareen@gmail.com>
2025-11-16 21:25:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Ini akan memindai semua file di folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
